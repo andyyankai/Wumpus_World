97a7d704995c2e7c8ec556c8c3be7c3d54ff90f5
andyyankai <kyan8@uci.edu>
2017-10-29 00:30:09 -0700

    dump ai

--- Wumpus_World_Python_Shell/src/MyAI.py
+++ Wumpus_World_Python_Shell/src/MyAI.py
@@ -18,7 +18,7 @@
 # ======================================================================
 
 from Agent import Agent
-
+import copy
 class MyAI ( Agent ):
 
 	def __init__ ( self ):
@@ -27,7 +27,7 @@ class MyAI ( Agent ):
         # ======================================================================
 		self.curr = {"loc" : [1, 1], "d" : 0, "wall" : ["W", "S"], "state" : []}
 		self.face = "E"
-		self.path = []
+		self.path = [{"loc" : [1, 1], "d" : 0, "wall" : ["W", "S"], "state" : []}]
 		self.gold = False
 		self.breeze = False
 		self.stench = False
@@ -43,20 +43,26 @@ class MyAI ( Agent ):
         # ======================================================================
         # YOUR CODE BEGINS
         # ======================================================================
-		self.path.append(self.curr)
-		if (stench or breeze) and self.curr == [1, 1]:
-			return Agent.Action.CLIMB
 		if glitter:
 			self.gold = True
+			self.back = True
 			return Agent.Action.GRAB
+		if self.curr["loc"] == [1, 1] and self.back == True:
+			return Agent.Action.CLIMB
+		if (stench or breeze) and self.curr["loc"] == [1, 1]:
+			return Agent.Action.CLIMB
+		if stench or breeze or self.back == True:
+			self.back = True
+			return self.backHome()
 		if not stench and not breeze and not glitter and not bump:
-			self.curr = [1, 1]
+			self.updateCurr()
+			self.path.append(copy.deepcopy(self.curr))
 			return Agent.Action.FORWARD
 		if bump:
+			self.updateFaceL()
 			return Agent.Action.TURN_LEFT
 
-		#if self.curr == [1, 1]:
-		#	return Agent.Action.CLIMB
+
         # ======================================================================
         # YOUR CODE ENDS
         # ======================================================================
@@ -65,10 +71,114 @@ class MyAI ( Agent ):
     # YOUR CODE BEGINS
     # ======================================================================
 	def updateCurr( self ):
-		if self.face = "E":
+		if self.face == "E":
 			self.curr["loc"][1] += 1
-		elif self.face = "W":
+		elif self.face == "W":
 			self.curr["loc"][1] -= 1
+		elif self.face == "N":
+			self.curr["loc"][0] += 1
+		elif self.face == "S":
+			self.curr["loc"][0] -= 1
+		self.curr["d"] = self.curr["loc"][0] + self.curr["loc"][1]
+	
+	def updateFaceL( self ):
+		if self.face == "E":
+			self.face = "N"
+		elif self.face == "W":
+			self.face = "S"
+		elif self.face == "N":
+			self.face = "W"
+		elif self.face == "S":
+			self.face = "E"
+			
+	def updateFaceR( self ):
+		if self.face == "E":
+			self.face = "S"
+		elif self.face == "W":
+			self.face = "N"
+		elif self.face == "N":
+			self.face = "E"
+		elif self.face == "S":
+			self.face = "W"
+
+	def backHome( self ):
+		print(self.face)
+		for node in self.path:
+			if node["d"] < self.curr["d"]:
+				if self.face == "E":
+					print("loli1")
+					if node["loc"][1] == self.curr["loc"][1] + 1 and node["loc"][0] == self.curr["loc"][0]:
+						self.path.remove(self.curr)
+						self.updateCurr()
+						return Agent.Action.FORWARD
+					elif node["loc"][1] == self.curr["loc"][1] -1 and node["loc"][0] == self.curr["loc"][0]:
+						self.updateFaceL()
+						return Agent.Action.TURN_LEFT
+					elif node["loc"][1] == self.curr["loc"][1] and node["loc"][0] == self.curr["loc"][0] + 1:
+						self.updateFaceR()
+						return Agent.Action.TURN_RIGHT
+					elif node["loc"][1] == self.curr["loc"][1] and node["loc"][0] == self.curr["loc"][0] - 1:
+						self.updateFaceL()
+						return Agent.Action.TURN_LEFT
+					
+				elif self.face == "W":
+					print("loli2")
+					if node["loc"][1] == self.curr["loc"][1] - 1 and node["loc"][0] == self.curr["loc"][0]:
+						print("1")
+						self.path.remove(self.curr)
+						self.updateCurr()
+						return Agent.Action.FORWARD
+					elif node["loc"][1] == self.curr["loc"][1] + 1 and node["loc"][0] == self.curr["loc"][0]:
+						print("2")
+						self.updateFaceL()
+						return Agent.Action.TURN_LEFT
+					elif node["loc"][1] == self.curr["loc"][1] and node["loc"][0] == self.curr["loc"][0] + 1:
+						print("3")
+						self.updateFaceR()
+						return Agent.Action.TURN_RIGHT
+					elif node["loc"][1] == self.curr["loc"][1] and node["loc"][0] == self.curr["loc"][0] - 1:
+						print("4")
+						self.updateFaceL()
+						return Agent.Action.TURN_LEFT
+				elif self.face == "N":
+					print("loli3")
+					if node["loc"][1] == self.curr["loc"][1] + 1 and node["loc"][0] == self.curr["loc"][0]:
+						print("1")
+						self.updateFaceR()
+						return Agent.Action.TURN_RIGHT
+					elif node["loc"][1] == self.curr["loc"][1] -1 and node["loc"][0] == self.curr["loc"][0]:
+						print("2")
+						self.updateFaceL()
+						return Agent.Action.TURN_LEFT
+					elif node["loc"][1] == self.curr["loc"][1] and node["loc"][0] == self.curr["loc"][0] + 1:
+						print("3")
+						self.path.remove(self.curr)
+						self.updateCurr()
+						return Agent.Action.FORWARD
+					elif node["loc"][1] == self.curr["loc"][1] and node["loc"][0] == self.curr["loc"][0] - 1:
+						print("4")
+						self.updateFaceL()
+						return Agent.Action.TURN_LEFT
+				elif self.face == "S":
+					print("loli4")
+					if node["loc"][1] == self.curr["loc"][1] + 1 and node["loc"][0] == self.curr["loc"][0]:
+						print("1")
+						self.updateFaceL()
+						return Agent.Action.TURN_LEFT
+					elif node["loc"][1] == self.curr["loc"][1] -1 and node["loc"][0] == self.curr["loc"][0]:
+						print("2")
+						self.updateFaceR()
+						return Agent.Action.TURN_RIGHT
+					elif node["loc"][1] == self.curr["loc"][1] and node["loc"][0] == self.curr["loc"][0] + 1:
+						print("3")
+						self.updateFaceL()
+						return Agent.Action.TURN_LEFT
+					elif node["loc"][1] == self.curr["loc"][1] and node["loc"][0] == self.curr["loc"][0] - 1:
+						print("4")
+						self.path.remove(self.curr)
+						self.updateCurr()
+						return Agent.Action.FORWARD
+		
     
     # ======================================================================
     # YOUR CODE ENDS
